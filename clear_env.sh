065b283b3d7605eeb50efef096a9ecf0c384282e
Carles San Agustin <csanagustin@worldsensing.com>
2017-07-25 17:00:32 +0200

    clear environment fix

---
+++ clear_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-compose down
+sudo rm -Rf data
+sudo rm -Rf sensu_ssl_tool*
+rm ssl_tool.sh
+
+
--- down_clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose down
-sudo rm -Rf data/*
-#docker rmi sensustack_sensu:latest
