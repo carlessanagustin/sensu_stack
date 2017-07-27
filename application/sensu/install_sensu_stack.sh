544605b7cd5675c5697a894fcbad799e55796712
Carles San Agustin <csanagustin@worldsensing.com>
2017-07-27 18:02:34 +0200

    rubygems package needed

--- application/sensu/install_sensu_stack.sh
+++ application/sensu/install_sensu_stack.sh
@@ -2,10 +2,14 @@
 
 apt-get update && \
 apt-get -y install wget apt-transport-https apt-utils && \
-apt-get -y install ruby ruby-dev build-essential && \
+apt-get -y install ruby ruby-dev build-essential rubygems && \
 wget -q https://sensu.global.ssl.fastly.net/apt/pubkey.gpg -O- | apt-key add - && \
 echo "deb  https://sensu.global.ssl.fastly.net/apt xenial main" > /etc/apt/sources.list.d/sensu.list && \
 apt-get update && apt-get -y install sensu && \
 sensu_plugins="sensu-plugin sensu-plugins-docker sensu-plugins-disk-checks sensu-plugins-slack sensu-plugins-network-checks sensu-plugins-kubernetes sensu-plugins-rabbitmq sensu-plugins-http sensu-plugins-mailer sensu-plugins-process-checks sensu-plugins-memory-checks sensu-plugins-ssl sensu-plugins-redis sensu-plugins-filesystem-checks sensu-plugins-cpu-checks sensu-plugins-logs sensu-plugins-load-checks sensu-plugins-jenkins sensu-plugins-sensu sensu-plugins-ansible sensu-plugins-apache sensu-plugins-uchiwa sensu-plugins-twilio sensu-plugins-graphite" && \
 for plugin in $sensu_plugins; do gem install $plugin; done && \
 gem install vmstat
+
+
+
+git- libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
